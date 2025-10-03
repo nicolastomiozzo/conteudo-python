ebe5da5fbefaf679f7fa8040f6f33d964c54c069
NICOLAS MACHADO TOMIOZZO <nicolas_tomiozzo@estudante.sesisenai.org.br>
2025-10-03 16:55:22 -0300

    Feito exercicios do 1 ao 4

---
+++ exercicios/sequencial/exercicio_3.py
@@ -0,0 +1,5 @@
+numero_1 = int(input("Digite o valor 1: "))
+numero_2 = int(input("Digite o valor 2: "))
+
+resultado = numero_1 + numero_2
+print("A soma do numero 1 com o numero 2 é:",resultado)
---
+++ exercicios/sequencial/exercicio_4.py
@@ -0,0 +1,11 @@
+nota_1 = float(input("Digite a primeira nota: "))
+nota_2 = float(input("Digite a segunda nota: "))
+nota_3 = float(input("Digite a terceira nota: "))
+nota_4 = float(input("Digite a quarta nota: "))
+
+soma = nota_1 + nota_2 + nota_3 + nota_4
+media = soma / 4
+
+media_outro = (nota_1 + nota_2 + nota_3 + nota_4) / 4
+
+print("A média do aluno é: ",media)
